e9f6687ecd826bec6bf0b6e397aa525696b96157
moonknight324 <jainmanya003@gmail.com>
2023-09-26 17:24:11 +0530

    hi

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score = localStorage.getItem("score");
+console.log(score); //debugging
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+
+playAgainButton.onclick = () => {
+    location.href = "./game.html";
+};
